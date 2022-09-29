83381d2f7d74e94a7b6b9c58cd08e9a5cc98a040
Nate Wheaton <57465272+npwheaton@users.noreply.github.com>
2022-09-29 14:56:33 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import Form from './components/Form';
+import TodoManager from './components/TodoManager';
+import InsertFormButton from './components/InsertFormButton';
+import AlertMessage from './components/AlertMessage';
+
+function App() {
+  const [alertmsg, setAlertMessage] = useState({message: "",showAlert: false}); 
+  const [showForm, setShowForm] = useState(false);  
+  const [inputText, setInputText] = useState("");
+  const [inputDate, setInputDate] = useState("");
+  const [inputTime, setInputTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [currentMilliSec, setcurrentMilliSec] = useState(Date.now());
+  const [listFilter, setListFilter] = useState("all");
+  useEffect(() => {
+    //get saved todos
+      if(localStorage.getItem("list")!==null){
+        let newlist = JSON.parse(localStorage.getItem("list"));
+        setTodos(newlist);
+      }
+      
+
+    const myInterval = setInterval(()=>{setcurrentMilliSec(Date.now())},60000);
+
+    return () => clearInterval(myInterval);
+  }, []);
+  
+  useEffect(() => {
+    //save todos
+    localStorage.setItem("list", JSON.stringify(todos));
+  }, [todos]);
+
+
+  
+  const changeForm=()=>{
+    let form = !showForm;
+    setShowForm(form);
+  }
+  const setAlert=(msg)=>{
+    setAlertMessage({message: msg, showAlert: true})
+  }
+  const calcTillDateDue=(singleTodo)=>{
+    let dtString = singleTodo.dateDue.split('|');
+    let ndate = new Date(`${dtString[0]}T${dtString[1]}:00`);
+    let milldue = ndate.getTime();
+    if(singleTodo.completed===true){
+      return  currentMilliSec;
+    }
+    return milldue - currentMilliSec;
+  }
+  const sortbydateDue=(a,b)=>{
+    if(calcTillDateDue(a) > calcTillDateDue(b)){
+      return 1;
+  } else if(calcTillDateDue(a) < calcTillDateDue(b)){
+      return -1;
+  }
+  return 0;
+  }
+
+
+// do all the css
+
+
+
+  
+  return (
+    <div className="App">
+        <h1>Action Board</h1>
+        {alertmsg.showAlert&& <AlertMessage setAlertMessage={setAlertMessage} message={alertmsg.message}/>}
+          {!showForm? <InsertFormButton  changeForm={changeForm}/>:
+          <Form 
+          setInputText={setInputText}
+          setInputDate={setInputDate}
+          setInputTime={setInputTime} 
+          setTodos={setTodos}
+          inputText={inputText}
+          inputDate={inputDate}
+          inputTime={inputTime}
+          todos={todos}
+          currentMilliSec={currentMilliSec}
+          changeForm={changeForm}
+          setAlert={setAlert}
+          showAlert={alertmsg.showAlert}
+          sortbydateDue={sortbydateDue}
+
+          
+        />
+        }
+        
+        <TodoManager
+        listFilter={listFilter}
+        todos={todos}
+        currentMilliSec={currentMilliSec}
+        setAlert={setAlert}
+        setTodos={setTodos}
+        setListFilter={setListFilter}
+        sortbydateDue={sortbydateDue}
+
+
+        />
+    </div>
+  );
+}
+
+export default App;
